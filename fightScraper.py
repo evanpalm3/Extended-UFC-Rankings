bf4470654e053eda93669865d1aa9ad2557bb46a
evanpalm3 <145077892+evanpalm3@users.noreply.github.com>
2024-07-01 19:51:45 -0700

    update

--- fightScraper.py
+++ fightScraper.py
@@ -1,5 +1,14 @@
 import requests
+import os
 from bs4 import BeautifulSoup
+
+filename = 'fights.txt'
+
+hasLast = os.path.exists(filename)
+file = open(filename,'r')
+lines=file.readlines()
+lastFight=lines[0].split('*')[4]
+file.close()
 
 page_to_scrape = requests.get("http://www.ufcstats.com/statistics/events/completed?page=all")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
@@ -11,11 +20,13 @@ for link in soup.findAll("a", attrs={"class":"b-link b-link_style_black"}):
 fights=[]
 num=0
 for link in links:
-    num+=1
-    print(str(num)+" of "+str(len(links)))
     page_to_scrape = requests.get(link)
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
     date = soup.find("div", attrs={"class":"b-list__info-box b-list__info-box_style_large-width"}).text.split('\n')[6].strip()
+    if(date==lastFight):
+        break
+    num+=1
+    print(str(num)+" of "+str(len(links)))
     rows = soup.findAll("tr", attrs={"class":"b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click"})
     for row in rows:
         parts = row.findAll("p", attrs={"class":"b-fight-details__table-text"})
@@ -32,11 +43,13 @@ for link in links:
         entry=[fighter1,fighter2,weightclass,result,date]
         fights.append(entry)
         
-file = open('fights.txt', mode='w')
+file = open(filename, mode='w')
 for fight in fights:
     for part in fight:
         file.write(part+"*")
     file.write("\n")
+for line in lines:
+    file.write(line)
 file.close()
 print("done")
 
